8f1596b15e7a766e13bdfae0bbdfa6396261b467
Andreas Amundsen <andreas.amundsen123@gmail.com>
2024-10-30 17:25:34 +0100

    Initial Commit

---
+++ ShoppingListTests/ShoppingListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListTests.swift
+//  ShoppingListTests
+//
+//  Created by Andreas Amundsen on 30/10/2024.
+//
+
+import Testing
+@testable import ShoppingList
+
+struct ShoppingListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
